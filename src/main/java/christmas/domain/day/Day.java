a4e440f8d011145523f3f2dcef65d5654da726a1
WinterHana <haha920@naver.com>
2023-11-13 04:22:55 +0900

    feat : Day에 대한 클래스 생성
    - 날짜를 받으면 무슨 날짜인지 확인하는 클래스 생성
    - 이와 관련된 DiscountDay 열거형 추가
    - README.md에 내용 반영

---
+++ src/main/java/christmas/domain/day/Day.java
@@ -0,0 +1,9 @@
+package christmas.domain.day;
+
+public class Day {
+    int day;
+
+    public Day(int day) {
+        this.day = day;
+    }
+}
---
+++ src/main/java/christmas/domain/day/DiscountDay.java
@@ -0,0 +1,8 @@
+package christmas.domain.day;
+
+public enum DiscountDay {
+    CHRISMAS,   // 크리스마스 할인
+    SPECIAL,    // 특별 할인
+    WEEKDAY,    // 주중 할인
+    WEEKEND;    // 주말 할인
+}
