456a76a6b916129454ee969a21f7f7d49e0b3144
WinterHana <haha920@naver.com>
2023-11-13 04:00:49 +0900

    feat : 추상클래스 Discount 추가
    - 할인에 대한 객체를 만들기 위해 상속할 추상 클래스 추가

--- src/main/java/christmas/domain/Account.java
+++ src/main/java/christmas/domain/Account.java
@@ -1,12 +1,18 @@
 package christmas.domain;
 
+import christmas.domain.discount.Discount;
+
+import java.util.List;
+
 /**
  * 총 계산서를 만든다.
  */
 public class Account {
     FoodGenerator foodGenerator;
+    List<Discount> discountDetail;
 
-    public Account(FoodGenerator foodGenerator) {
+    public Account(FoodGenerator foodGenerator, List<Discount> discountDetail) {
         this.foodGenerator = foodGenerator;
+        this.discountDetail = discountDetail;
     }
 }
---
+++ src/main/java/christmas/domain/discount/Discount.java
@@ -0,0 +1,7 @@
+package christmas.domain.discount;
+
+public abstract class Discount {
+    int discountPrice;
+
+    public abstract int discount();
+}
